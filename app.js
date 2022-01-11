e4ed8a3f3198484df96e22b5404dee2561ec875f
Kevin-Solomon <kevinsolomon404@gmail.com>
2022-01-11 20:08:59 +0530

    refactor - changed from ES5 to ES6

--- app.js
+++ app.js
@@ -2,25 +2,29 @@ const initialPrice = document.querySelector('#initial-price')
 const quantity = document.querySelector('#quantity')
 const currPrice = document.querySelector('#current-price')
 const outputDiv = document.querySelector('#output-box')
-document.addEventListener('submit', submitHandler)
-function submitHandler(e) {
+
+//ES6 Implimentation
+document.addEventListener('submit', e=>{
   e.preventDefault()
-  let ip = Number(initialPrice.value)
-  let qty = Number(quantity.value)
-  let curr = Number(currPrice.value)
+  let ip = +initialPrice.value
+  let qty = +quantity.value
+  let curr = +currPrice.value
   calculateProfitLoss(ip, qty, curr)
-}
-function calculateProfitLoss(initialPrice, quantity, currPrice) {
+})
+
+//ES6 Implimentation
+
+const calculateProfitLoss = (initialPrice, quantity, currPrice)=>{
   if (initialPrice > currPrice) {
     let loss = (initialPrice - currPrice) * quantity;
     let lossPercentage = ((initialPrice - currPrice) * 100)/initialPrice;
-    outputDiv.innerText = (`Hey the loss is ${loss.toFixed(2)} and loss percentage is ${lossPercentage.toFixed(2)}%`)
+    outputDiv.innerText = `Hey the loss is ${loss.toFixed(2)} and loss percentage is ${lossPercentage.toFixed(2)}%`
 
   }
   else if (currPrice > initialPrice) {
     let profit = (currPrice - initialPrice) * quantity;
     let profitPercentage =  ((currPrice - initialPrice) * 100) / initialPrice;
-    outputDiv.innerText = (`Hey the profit is ${profit.toFixed(2)} and profit percentage is ${profitPercentage.toFixed(2)}%`)
+    outputDiv.innerText = `Hey the profit is ${profit.toFixed(2)} and profit percentage is ${profitPercentage.toFixed(2)}%`
 
   }
   else {
